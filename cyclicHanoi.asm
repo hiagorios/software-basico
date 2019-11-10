6ebeab628e8d32d92d96987e213c91be4b3c78c9
Hiago Rios <hiagoriosc@gmail.com>
2019-11-10 17:48:16 -0300

    Funcionando de algum jeito

--- cyclicHanoi.asm
+++ cyclicHanoi.asm
@@ -11,12 +11,11 @@ section .data
                           db        "ORIGEM: "                      
         pino_origem:      db        " "  
                           db        " | DESTINO: "     
-        pino_destino:     db        " ", 0xa  
-        lenght            equ       $-msg
+        pino_destino:     db        " ", NULL
 
 section .bss
 
-    qntDiscos resb 5
+    qntDiscos resb 2
     
 section .text
 
@@ -48,7 +47,6 @@ section .text
         push dword 0x1                  ; ''    ''    '' origem na pilha
         push eax                        ; Colocando a qnt de discos na pilha
 
-        ;call funcaoHanoi
         call clock
 
         ;Fim
@@ -74,6 +72,7 @@ section .text
         jle fim
         ;------------------------------
 
+
         ;-------RECURSIVIDADE---------
         ;Colocando o que será o pino auxiliar na pilha
         push dword [ebp+16]
@@ -81,15 +80,16 @@ section .text
         push dword [ebp+20]
         ;Colocando o que será o pino de origem na pilha
         push dword [ebp+12]
-        ;Decrementa n e poe na pilha
+        ;Poe n-1 na pilha
         dec eax
         push dword eax
         call anti
         ;-------RECURSIVIDADE---------
 
+        ;Faz um pop de 4x4bytes
+        add esp,16
+
         ;-------IMPRIME MOVIMENTO ------
-        ;Faz um pop de 3x4bytes
-        add esp,12
         ;Colocando o que será o pino de destino na pilha
         push dword [ebp+16]
         ;Colocando o que será o pino de origem na pilha
@@ -99,9 +99,10 @@ section .text
         call imprimePasso
         ;-------IMPRIME MOVIMENTO ------
         
-        ;-------RECURSIVIDADE---------
         ;Faz um pop de 3x4bytes
         add esp,12
+
+        ;-------RECURSIVIDADE---------
         ;Colocando o que será o pino auxiliar na pilha
         push dword [ebp+12]
         ;Colocando o que será o pino de destino na pilha
@@ -115,7 +116,10 @@ section .text
         push dword eax
         call anti
         ;-------RECURSIVIDADE---------
-        ret
+
+        ;Faz um pop de 4x4bytes
+        add esp,16
+        jmp fim
 
     ;anti (n, from, to, aux)
     ;n = ebp+8
@@ -150,11 +154,12 @@ section .text
         call anti
         ;-------RECURSIVIDADE---------
 
+        ;Faz um pop de 5x4bytes
+        add esp,16
+
         ;-------IMPRIME MOVIMENTO ------
-        ;Faz um pop de 3x4bytes
-        add esp,12
         ;Colocando o que será o pino de destino na pilha
-        push dword [ebp+16]
+        push dword [ebp+20]
         ;Colocando o que será o pino de origem na pilha
         push dword [ebp+12]
         ;Coloca n atual na pilha
@@ -162,9 +167,10 @@ section .text
         call imprimePasso
         ;-------IMPRIME MOVIMENTO ------
         
-        ;-------RECURSIVIDADE---------
         ;Faz um pop de 3x4bytes
         add esp,12
+
+        ;-------RECURSIVIDADE---------
         ;Colocando o que será o pino auxiliar na pilha
         push dword [ebp+20]
         ;Colocando o que será o pino de destino na pilha
@@ -179,17 +185,21 @@ section .text
         call clock
         ;-------RECURSIVIDADE---------
 
+        ;Faz um pop de 4x4bytes
+        add esp,16
+
         ;-------IMPRIME MOVIMENTO ------
-        ;Faz um pop de 3x4bytes
-        add esp,12
         ;Colocando o que será o pino de destino na pilha
-        push dword [ebp+20] ;talvez nao esteja certo isso
+        push dword [ebp+16]
         ;Colocando o que será o pino de origem na pilha
-        push dword [ebp+12]
+        push dword [ebp+20]
         ;Coloca n atual na pilha
         push dword [ebp+8]
         call imprimePasso
         ;-------IMPRIME MOVIMENTO ------
+
+        ;Faz um pop de 3x4bytes
+        add esp,12
 
         ;-------RECURSIVIDADE---------
         ;Colocando o que será o pino auxiliar na pilha
@@ -203,7 +213,10 @@ section .text
         push dword eax
         call anti
         ;-------RECURSIVIDADE---------
-        ret
+
+        ;Faz um pop de 4x4bytes
+        add esp,16
+        jmp fim
 
     fim:
         ;Voltando à stack frame anterior
@@ -218,15 +231,15 @@ section .text
         mov ebp,esp
         ;---------------------------
 
-        ;Pino auxiliar
-        mov eax, [ebp + 12]
+        ;Pino de origem
+        mov eax, [ebp+12]
         ;Conversao para ASCII
         add al, '0'
         ;Movimento: movendo o conteudo de al para [pino_origem]
         mov [pino_origem], al
 
         ;Pino de destino
-        mov eax, [ebp + 16]
+        mov eax, [ebp+16]
         ;Conversao para ASCII
         add al, '0'
         ;Movimento: movendo o conteudo de al para [pino_destino]
@@ -234,71 +247,13 @@ section .text
 
         ;Imprime a mensagem de movimento de disco
         mov eax, msg
-        mov ebx, lenght
         call stringPrintLF
+
+        mov eax, [ebp+8]
+        call intPrintLF
 
         ;Voltando à stack frame anterior
         mov esp,ebp
         pop ebp
         ;--------------------------------
         ret
-
-    funcaoHanoi: 
-        ;[ebp+8] = n (número de discos iniciais)
-        ;[ebp+12] = pino de origem
-        ;[ebp+16] = pino auxiliar
-        ;[ebp+20] = pino de destino
-        ;link: http://www.devmedia.com.br/torres-de-hanoi-solucao-recursiva-em-java/23738
-
-        ;Criando uma nova stack frame
-        push ebp
-        mov ebp,esp
-        ;---------------------------
-
-        ;eax recebe o valor do primeiro elemento da pilha (n atual)
-        mov eax,[ebp+8]
-        ;Compara n atual com 0x0 = 0 em hexadecimal
-        cmp eax,0x0
-        ;Se n <= 0, é o caso base e vai para o fim dessa chamada
-        jle fim
-        
-        ;PASSO1 - RECURSIVIDADE
-        ;Decrementa 1 de eax (n atual)
-        dec eax            
-        ;Coloca o pino auxiliar na pilha
-        push dword [ebp+16]
-        ;Coloca o pino de destino na pilha
-        push dword [ebp+20]
-        ;Coloca o pino de origem na pilha
-        push dword [ebp+12]
-        ;Poe eax na pilha como parâmetro n, já com -1 para a recursividade
-        push dword eax
-        ;Chama a funcao recursivamente
-        call funcaoHanoi
-
-        ;PASSO2 - MOVER PINO E IMPRIMIR
-        ;Faz um pop de 3x4bytes - Último e primeiro parâmetro
-        add esp,12
-        ; pega o pino de origem referenciado pelo parâmetro ebp+16
-        push dword [ebp+16]
-        ; coloca na pilha o pino de origem
-        push dword [ebp+12]
-        ; coloca na pilha o pino de o numero de disco inicial
-        push dword [ebp+8]
-        call imprimePasso
-        
-        ;PASSO3 - RECURSIVIDADE
-        ;Faz um pop de 3x4bytes - Último e primeiro parâmetro
-        add esp,12
-        ;Coloca o pino de origem na pilha
-        push dword [ebp+12]
-        ;Coloca o pino auxiliar na pilha
-        push dword [ebp+16]
-        ;Coloca o pino de destino na pilha
-        push dword [ebp+20]
-        ;Move para o registrador eax o espaço reservado ao número de discos atuais
-        mov eax,[ebp+8]
-        ;Decrementa 1 de eax (n atual)
-        dec eax
-        push dword eax                  ; poe eax na pilha
-        call funcaoHanoi                ; (recursividade)
