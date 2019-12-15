b478aa216ca228ae8dacfb58162d364e832132c9
Hiago Rios <hiagoriosc@gmail.com>
2019-12-15 11:08:14 -0300

    Inicio do projeto do montador

---
+++ Trabalhos/2/Montador/src/montador/Assembler.java
@@ -0,0 +1,17 @@
+package montador;
+
+/**
+ *
+ * @author Hiago Rios Cordeiro
+ * @author João Henrique dos Santos Queiroz
+ */
+public class Assembler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
