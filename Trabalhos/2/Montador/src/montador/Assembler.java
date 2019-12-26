8ac5a0ab7941d7995db163a2ae4a1f5591a02515
Hiago Rios <hiagoriosc@gmail.com>
2019-12-26 17:22:41 -0300

    Tratamento das linhas e output em arquivo

--- Trabalhos/2/Montador/src/montador/Assembler.java
+++ Trabalhos/2/Montador/src/montador/Assembler.java
@@ -1,7 +1,9 @@
 package montador;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,25 +15,38 @@ import java.util.Scanner;
  */
 public class Assembler {
 
-    private HashMap<Integer, String> hashmap = new HashMap<>();
-    private List<String> lines = new ArrayList<>();
-    private List<String>
+    private static HashMap<Integer, String> hashmap = new HashMap<>();
+    private static List<String> lines = new ArrayList<>();
 
     public static void main(String[] args) {
         Assembler asb = new Assembler();
         asb.readSource("cyclicHanoi.asm");
+        asb.writeFile("output.txt");
     }
 
     public void readSource(String path) {
         Scanner reader;
         try {
-            reader = new Scanner(new FileReader(path));
-            while (reader.hasNextLine()) {
-                lines.add(reader.nextLine());
+            reader = new Scanner(new FileReader("src/montador/" + path));
+            Boolean cont = true;
+            while (cont) {
+                if (reader.hasNextLine()) {
+                    String line = removeComments(reader.nextLine());
+                    line = removeIdentation(line);
+                    if (line != null && !line.isEmpty()) {
+                        if (isInclude(line)) {
+                            readSource(getIncludeFile(line));
+                        } else {
+                            lines.add(line);
+                        }
+                    }
+                } else {
+                    cont = false;
+                    reader.close();
+                }
             }
-            reader.close();
         } catch (FileNotFoundException e) {
-            System.out.println("Source file not found.");
+            System.out.println("Source file not found: " + path);
             System.exit(0);
         }
     }
@@ -39,10 +54,10 @@ public class Assembler {
     public void readOPCodes(String path) {
         Scanner reader;
         try {
-            reader = new Scanner(new FileReader(path));
+            reader = new Scanner(new FileReader("src/montador/" + path));
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
-                
+
                 lines.add(line);
             }
             reader.close();
@@ -52,14 +67,41 @@ public class Assembler {
         }
     }
 
+    public boolean isInclude(String line) {
+        // Testar
+        return line.contains("%include");
+    }
+
     public boolean isLabel(String line) {
         //Testar
         return !line.contains("_") && line.contains(":");
     }
 
-    public String removeComment(String line) {
+    public String removeComments(String line) {
         //Testar
         return line.split(";")[0];
     }
 
+    public String removeIdentation(String line) {
+        // Testar
+        return line.trim().replaceAll(" +", " ");
+    }
+
+    public String getIncludeFile(String line) {
+        // Testar
+        return line.split("'")[1].split("'")[0];
+    }
+
+    public void writeFile(String name) {
+        try {
+            PrintWriter writer = new PrintWriter(new File("src/montador/" + name));
+            for (String line : lines) {
+                writer.println(line);
+            }
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not create the file " + name);
+            System.exit(-1);
+        }
+    }
 }
